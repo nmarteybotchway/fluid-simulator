dbac72d468534039fb0a720d5242078971d71858
Neil Martey-Botchway <neil.marteybotchway@gmail.com>
2025-08-30 20:44:02 +0100

    Got first iteration of fluid simulation to work

--- Fluid.h
+++ Fluid.h
@@ -1,13 +1,10 @@
-//
-// Created by neil on 8/30/25.
-//
-
 #ifndef FLUID_SIMULATION_FLUID_H
 #define FLUID_SIMULATION_FLUID_H
+
 #include <cmath>
 #include <vector>
 #include <SFML/Graphics.hpp>
-#include <SFML/System/Vector2.hpp>
+#include <algorithm>
 
 class Fluid {
     int N;
@@ -27,81 +24,73 @@ class Fluid {
 
 public:
     Fluid(int size, int iters, float dt, float diffusion, float viscosity) :
-    N{size}, iters{iters}, dt{dt}, diff{diffusion}, visc{viscosity} {
-        this->s.resize(N*N, 0.0f);
-        this->density.resize(N*N, 0.0f);
+        N{size}, iters{iters}, dt{dt}, diff{diffusion}, visc{viscosity} {
+        s.resize(N*N, 0.0f);
+        density.resize(N*N, 0.0f);
 
-        this->Vx.resize(N*N, 0.0f);
-        this->Vy.resize(N*N, 0.0f);
+        Vx.resize(N*N, 0.0f);
+        Vy.resize(N*N, 0.0f);
 
-        this->Vx0.resize(N*N, 0.0f);
-        this->Vy0.resize(N*N, 0.0f);
+        Vx0.resize(N*N, 0.0f);
+        Vy0.resize(N*N, 0.0f);
     }
 
     inline int IX(int x, int y) {
+        x = std::clamp(x, 0, N -1);
+        y = std::clamp(y, 0, N -1);
         return x + y * N;
     }
+
     void addDensity(int x, int y, float amount) {
-        int index = IX(x, y);
-        density[index] += amount;
+        density[IX(x, y)] += amount;
     }
 
     void addVelocity(int x, int y, float amountX, float amountY) {
-        int index = IX(x, y);
-        Vx[index] += amountX;
-        Vy[index] += amountY;
+        Vx[IX(x, y)] += amountX;
+        Vy[IX(x, y)] += amountY;
     }
 
-    void set_bnd(int b, std::vector<float> x)
-    {
+    void set_bnd(int b, std::vector<float>& x) {
         for (int i = 1; i < N - 1; i++) {
             x[IX(i, 0)] = b == 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
             x[IX(i, N - 1)] = b == 2 ? -x[IX(i, N - 2)] : x[IX(i, N - 2)];
         }
-
-        for(int j = 1; j < N - 1; j++) {
-            x[IX(0  , j)] = b == 1 ? -x[IX(1  ,j)] : x[IX(1  , j)];
-            x[IX(N-1, j)] = b == 1 ? -x[IX(N-2, j)] : x[IX(N-2, j)];
+        for (int j = 1; j < N - 1; j++) {
+            x[IX(0, j)] = b == 1 ? -x[IX(1, j)] : x[IX(1, j)];
+            x[IX(N - 1, j)] = b == 1 ? -x[IX(N - 2, j)] : x[IX(N - 2, j)];
         }
 
-
-        x[IX(0, 0)] = 0.5f * (x[IX(1, 0)]+ x[IX(0, 1)]);
+        x[IX(0, 0)] = 0.5f * (x[IX(1, 0)] + x[IX(0, 1)]);
         x[IX(0, N-1)] = 0.5f * (x[IX(1, N-1)] + x[IX(0, N-2)]);
         x[IX(N-1, 0)] = 0.5f * (x[IX(N-2, 0)] + x[IX(N-1, 1)]);
         x[IX(N-1, N-1)] = 0.5f * (x[IX(N-2, N-1)] + x[IX(N-1, N-2)]);
     }
 
-    void lin_solve(int b, std::vector<float> x, std::vector<float>x0, float a, float c)
-    {
-        float cRecip = 1.0 / c;
+    void lin_solve(int b, std::vector<float>& x, const std::vector<float>& x0, float a, float c) {
+        float cRecip = 1.0f / c;
         for (int k = 0; k < iters; k++) {
-                for (int j = 1; j < N - 1; j++) {
-                    for (int i = 1; i < N - 1; i++) {
-                        x[IX(i, j)] =
-                            (x0[IX(i, j)]
-                                + a*(    x[IX(i+1, j)]
-                                        +x[IX(i-1, j)]
-                                        +x[IX(i  , j+1)]
-                                        +x[IX(i  , j-1)]
-                               )) * cRecip;
-                    }
+            for (int j = 1; j < N - 1; j++) {
+                for (int i = 1; i < N - 1; i++) {
+                    x[IX(i, j)] =
+                        (x0[IX(i, j)]
+                        + a*(x[IX(i+1, j)] + x[IX(i-1, j)]
+                             + x[IX(i, j+1)] + x[IX(i, j-1)])) * cRecip;
                 }
+            }
             set_bnd(b, x);
         }
     }
 
-    void project(std::vector<float> velocX, std::vector<float> velocY, std::vector<float> p, std::vector<float> div)
-    {
-            for (int j = 1; j < N - 1; j++) {
-                for (int i = 1; i < N - 1; i++) {
-                    div[IX(i, j)] = -0.5f*(
-                             velocX[IX(i+1, j)]
-                            -velocX[IX(i-1, j)]
-                            +velocY[IX(i  , j+1)]
-                            -velocY[IX(i  , j-1)]
-                        )/N;
-                    p[IX(i, j)] = 0;
-                }
+    void project(std::vector<float>& velocX, std::vector<float>& velocY,
+                 std::vector<float>& p, std::vector<float>& div) {
+        for (int j = 1; j < N - 1; j++) {
+            for (int i = 1; i < N - 1; i++) {
+                div[IX(i, j)] = -0.5f*(
+                    velocX[IX(i+1, j)] - velocX[IX(i-1, j)]
+                    + velocY[IX(i, j+1)] - velocY[IX(i, j-1)]
+                ) / N;
+                p[IX(i, j)] = 0;
+            }
         }
         set_bnd(0, div);
         set_bnd(0, p);
@@ -109,29 +98,15 @@ public:
 
         for (int j = 1; j < N - 1; j++) {
             for (int i = 1; i < N - 1; i++) {
-                velocX[IX(i, j)] -= 0.5f * (  p[IX(i+1, j)]
-                                                -p[IX(i-1, j)]) * N;
-                velocY[IX(i, j)] -= 0.5f * (  p[IX(i, j+1)]
-                                                -p[IX(i, j-1)]) * N;
+                velocX[IX(i, j)] -= 0.5f * (p[IX(i+1, j)] - p[IX(i-1, j)]) * N;
+                velocY[IX(i, j)] -= 0.5f * (p[IX(i, j+1)] - p[IX(i, j-1)]) * N;
             }
         }
         set_bnd(1, velocX);
         set_bnd(2, velocY);
     }
 
-    void step()
-    {
-        int N          = this->N;
-        float visc     = this->visc;
-        float diff     = this->diff;
-        float dt       = this->dt;
-        std::vector<float> Vx = this->Vx;
-        std::vector<float> Vy= this->Vy;
-        std::vector<float> Vx0     = this->Vx0;
-        std::vector<float> Vy0     = this->Vy0;
-        std::vector<float> s       = this->s;
-        std::vector<float> density = this->density;
-
+    void step() {
         diffuse(1, Vx0, Vx, visc, dt);
         diffuse(2, Vy0, Vy, visc, dt);
 
@@ -145,80 +120,58 @@ public:
         diffuse(0, s, density, diff, dt);
         advect(0, density, s, Vx, Vy, dt);
     }
-    void advect(int b, std::vector<float> d, std::vector<float> d0,  std::vector<float> velocX,
-        std::vector<float> velocY, float dt)
-    {
-        float i0, i1, j0, j1;
 
+    void advect(int b, std::vector<float>& d, const std::vector<float>& d0,
+                const std::vector<float>& velocX, const std::vector<float>& velocY, float dt) {
+        float Nfloat = N;
         float dtx = dt * (N - 2);
         float dty = dt * (N - 2);
+        for (int j = 1; j < N - 1; j++) {
+            for (int i = 1; i < N - 1; i++) {
+                float x = i - dtx * velocX[IX(i,j)];
+                float y = j - dty * velocY[IX(i,j)];
 
-        float s0, s1, t0, t1;
-        float tmp1, tmp2, x, y;
+                if (x < 0.5f) x = 0.5f;
+                if (x > Nfloat - 1.5f) x = Nfloat - 1.5f;
+                if (y < 0.5f) y = 0.5f;
+                if (y > Nfloat - 1.5f) y = Nfloat - 1.5f;
 
-        float Nfloat = N;
-        float ifloat, jfloat;
-        int i, j;
+                int i0 = static_cast<int>(floor(x));
+                int i1 = i0 + 1;
+                int j0 = static_cast<int>(floor(y));
+                int j1 = j0 + 1;
 
-            for(j = 1, jfloat = 1; j < N - 1; j++, jfloat++) {
-                for(i = 1, ifloat = 1; i < N - 1; i++, ifloat++) {
-                    tmp1 = dtx * velocX[IX(i, j)];
-                    tmp2 = dty * velocY[IX(i, j)];
-                    x    = ifloat - tmp1;
-                    y    = jfloat - tmp2;
+                float s1 = x - i0;
+                float s0 = 1.0f - s1;
+                float t1 = y - j0;
+                float t0 = 1.0f - t1;
 
-                    if(x < 0.5f) x = 0.5f;
-                    if(x > Nfloat + 0.5f) x = Nfloat + 0.5f;
-                    i0 = std::floor(x);
-                    i1 = i0 + 1.0f;
-                    if(y < 0.5f) y = 0.5f;
-                    if(y > Nfloat + 0.5f) y = Nfloat + 0.5f;
-                    j0 = std::floor(y);
-                    j1 = j0 + 1.0f;
-
-                    s1 = x - i0;
-                    s0 = 1.0f - s1;
-                    t1 = y - j0;
-                    t0 = 1.0f - t1;
-
-                    int i0i = static_cast<int>(i0);
-                    int i1i = static_cast<int>(i1);
-                    int j0i = static_cast<int>(j0);
-                    int j1i = static_cast<int>(j1);
-
-                    d[IX(i, j)] =
-                        s0 * (t0 * d0[IX(i0i, j0i)] + t1 * d0[IX(i0i, j1i)]) +
-                        s1 * (t0 * d0[IX(i1i, j0i)] + t1 * d0[IX(i1i, j1i)]);
-                    ;
-                }
+                d[IX(i,j)] =
+                    s0*(t0*d0[IX(i0,j0)] + t1*d0[IX(i0,j1)]) +
+                    s1*(t0*d0[IX(i1,j0)] + t1*d0[IX(i1,j1)]);
             }
+        }
         set_bnd(b, d);
     }
 
-
-    void diffuse (int b, std::vector<float> x, std::vector<float> x0,
-    float diff, float dt)
-    {
+    void diffuse(int b, std::vector<float>& x, const std::vector<float>& x0,
+                 float diff, float dt) {
         float a = dt * diff * (N - 2) * (N - 2);
-        lin_solve(b, x, x0, a, 1 + 6 * a);
+        lin_solve(b, x, x0, a, 1 + 6*a);
     }
 
-    void renderD(sf::RenderWindow &window) {
-        /// REFACTOR
+    void renderD(sf::RenderWindow& window) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                float x = i * 10;
-                float y = j * 10;
-                float d = this->density[IX(i, j)];
-                sf::RectangleShape cell{sf::Vector2f(10,10)};
-                cell.setPosition(x, y);
-                cell.setFillColor(sf::Color(d, d,d));
+                float d = density[IX(i,j)];
+                sf::Uint8 c = static_cast<sf::Uint8>(std::clamp(d, 0.0f, 255.0f));
+                sf::RectangleShape cell(sf::Vector2f(10,10));
+                cell.setPosition(i*10, j*10);
+                cell.setFillColor(sf::Color(c,c,c));
                 window.draw(cell);
             }
         }
     }
-
 };
-
 
 #endif //FLUID_SIMULATION_FLUID_H
--- main.cpp
+++ main.cpp
@@ -3,39 +3,53 @@
 #include "Fluid.h"
 
 int main() {
-    int n{64};
+    int n = 64;
     int scale = 10;
-    Fluid fluid = Fluid(n, 4, 00.1, 0, 0);
-    sf::RenderWindow window(sf::VideoMode(n*scale, n*scale), "Fluid Simulation");
-    bool isMousePressed{false};
+    Fluid fluid(n, 4, 0.1, 0, 0);
+    sf::RenderWindow window(sf::VideoMode(n * scale, n * scale), "Fluid Simulation");
+
+    sf::Vector2i prevMousePos = sf::Mouse::getPosition(window);
+    bool firstFrame = true;
+    bool isMousePressed = false;
+
     while (window.isOpen()) {
-        sf::Event event{};
+        sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed)
                 window.close();
-            }
 
-            if (event.type == sf::Event::MouseButtonPressed) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
-                    isMousePressed = true;
-                    fluid.addDensity(event.mouseButton.x/scale, event.mouseButton.y/scale, 100);
-                }
-            }
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+                isMousePressed = true;
 
-            if (event.type == sf::Event::MouseButtonReleased) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
-                    isMousePressed = false;
-                }
-            }
+            if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
+                isMousePressed = false;
         }
 
+        // Get current mouse position every frame
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+        // Compute delta (velocity)
+        float amtX = 0.0f;
+        float amtY = 0.0f;
+        if (!firstFrame) {
+            amtX = static_cast<float>(mousePos.x - prevMousePos.x);
+            amtY = static_cast<float>(mousePos.y - prevMousePos.y);
+        } else {
+            firstFrame = false;
+        }
+        prevMousePos = mousePos;
+
+        // Apply to fluid if left mouse is pressed
         if (isMousePressed) {
-            sf::Vector2i mousePos{sf::Mouse::getPosition(window)};
-            fluid.addDensity(mousePos.x/scale, mousePos.y/scale, 100);
+            fluid.addDensity(mousePos.x / scale, mousePos.y / scale, 100);
+            fluid.addVelocity(mousePos.x / scale, mousePos.y / scale, amtX, amtY);
+            std::cout << amtX <<amtY <<std::endl;
         }
 
+        // Step the fluid simulation
         fluid.step();
 
+        // Render
         window.clear();
         fluid.renderD(window);
         window.display();
